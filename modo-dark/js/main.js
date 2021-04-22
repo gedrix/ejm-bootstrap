daf43b2da29895abbcd07aade6ab4b2116ba9cce
Gerardo Ramirez <gedrix.rm93@gmail.com>
2021-04-22 15:17:07 -0500

    finalizacion dark-mode

--- modo-dark/js/main.js
+++ modo-dark/js/main.js
@@ -1,6 +1,22 @@
-const btnSwitch = document.querySelector('#switch');
+const btnSwitch = document.querySelector('#boton-dark');
 
 btnSwitch.addEventListener('click', ()=> {
 document.body.classList.toggle('dark');
-btnSwitch.classList.toggle('active');
+btnSwitch.classList.toggle('activar');
+
+// Guardamos el modo en localstorage.
+if(document.body.classList.contains('dark')){
+    localStorage.setItem('dark-mode', 'true');
+} else {
+    localStorage.setItem('dark-mode', 'false');
+}
 });
+
+// Obtenemos el modo actual.
+if(localStorage.getItem('dark-mode') === 'true'){
+	document.body.classList.add('dark');
+	btnSwitch.classList.add('active');
+} else {
+	document.body.classList.remove('dark');
+	btnSwitch.classList.remove('active');
+}
