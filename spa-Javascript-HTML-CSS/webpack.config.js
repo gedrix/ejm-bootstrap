85357bc36c5bf38870c17be4195be5a495378925
Gerardo Ramirez <gedrix.rm93@gmail.com>
2021-04-21 23:04:39 -0500

    spa initial

---
+++ spa-Javascript-HTML-CSS/webpack.config.js
@@ -0,0 +1,21 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+              use: ["style-loader", "css-loader"],
+            }
+        ]
+    }, 
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
